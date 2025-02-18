a1b7967bfe7e3941338517ab67352fb8d74dad88
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-18 11:09:43 +0300

    fix(vibrate): fix vibrate api usage This message: *   Uses the `fix` type because it corrects an issue in the code. *   Uses the `vibrate` scope because it specifically addresses changes related to the vibrate functionality. *   Provides a concise description of the change:  fix vibrate api usage  indicating a correction in how the vibrate API is used.

--- app.js
+++ app.js
@@ -30,8 +30,7 @@ counter.addEventListener("click", function count() {
   let numbercounter = parseInt(counter.textContent);
   numbercounter += 1;
   counter.textContent = numbercounter;
-  window.navinavigator.vibrate(500);
-  console.log(window);
+  window.navigator.vibrate(500);
 });
 
 let reset = document.querySelector(".reset");
