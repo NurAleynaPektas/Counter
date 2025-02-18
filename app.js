dc6d70b66c60379f1017a1670ba4cf2ce43b9a44
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-18 11:45:52 +0300

    fix(app): Remove empty block after querySelector

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ const counterChange = () => {
   navigator.vibrate(350); // titreşim süresine arttırılabilir.
 };
 counter.addEventListener("click", counterChange);
-const counterArea = document.querySelector(".btn")
+const counterArea = document.querySelector(".btn"){
+  
+}
 d.addEventListener("click", counterChange);
 
 let reset = document.querySelector(".reset");
